cde0c34db2e4f6a98fd06e84063ee1e16e31572b
Lucas <1609853@034LAB3028>
2017-10-23 22:12:17 -0200

    Versão incompleta

---
+++ MeuPrimeiroBackendN/Web/Models/Contexto/MeuContexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models.Contexto
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base("strConn")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Web.Models.TipoItem> TipoItems { get; set; }
+
+        public System.Data.Entity.DbSet<Web.Models.Item> Items { get; set; }
+    }
+}
